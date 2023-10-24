d068a3c33055f9618d2cd901ea15175dc4fddb7d
LucasMig <lucasmigliori@gmail.com>
2023-10-24 18:11:26 -0300

    replacing assets

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
-import styles from "./page.module.css";
+import React from "react";
+
+import styles from "./page.module.scss";
 
 export default function Home() {
   return (
